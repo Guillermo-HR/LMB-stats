44dc9ecc48ac02ba5fa7e5911cf60eb7720e9fde
guillermo-hr <guillermo.hre@gmail.com>
2025-06-01 16:06:54 -0600

    agregar querys

--- dB/juegosPorTemporada.sql
+++ dB/juegosPorTemporada.sql
@@ -1,11 +1,4 @@
-select equipo_id 
-from equipo;
-
-SELECT j.temporada, e.nombre,
-       COUNT(CASE WHEN j.local_id = e.equipo_id THEN 1 END) as juegos_local,
-       COUNT(CASE WHEN j.visitante_id = e.equipo_id THEN 1 END) as juegos_visitante,
-       COUNT(*) as total_partidos_equipo
-FROM equipo e
-LEFT JOIN juego j ON (e.equipo_id = j.local_id OR e.equipo_id = j.visitante_id) 
-GROUP BY j.temporada, e.nombre
-ORDER BY j.temporada, e.nombre;
+select temporada, count(*)
+from juego
+group by temporada
+order by temporada;
