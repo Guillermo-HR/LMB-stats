73f72301f20ca2b61bd5bd2df78129f2bbf5d1bf
guillermo-hr <guillermo.hre@gmail.com>
2025-06-02 22:28:07 -0600

    dividir el resultado para que sea mas facil de leer

--- dB/pruebaCount.sql
+++ dB/pruebaCount.sql
@@ -4,12 +4,12 @@ SELECT
 	(SELECT COUNT(*) FROM juego) AS juego,
 	(SELECT COUNT(*) FROM juego_pitcher) AS juego_pitcher,
 	(SELECT COUNT(*) FROM jugador) AS jugador,
-	(SELECT COUNT(*) FROM lanzamiento) AS lanzamiento,
+	(SELECT COUNT(*)/1000 FROM lanzamiento) AS lanzamiento,
     (SELECT COUNT(*) FROM posicion) AS posicion,
     (SELECT COUNT(*) FROM status_juego) AS status_juego,
 	(SELECT COUNT(*) FROM tipo_turno) AS tipo_turno,
     (SELECT COUNT(*) FROM tipo_juego) AS tipo_juego,
-	(SELECT COUNT(*) FROM turno) AS turno,
+	(SELECT COUNT(*)/1000 FROM turno) AS turno,
 	(SELECT COUNT(*) FROM umpire) AS umpire;
 
 SELECT EXTRACT(year FROM primer_lanzamiento) AS año, count(*)
